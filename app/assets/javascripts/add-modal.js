7734814e402b87f6894ef10349e591b521ac0ba5
minsikzzang <minsikzzang@hotmail.com>
2012-04-05 01:50:24 +0200

    page design added for 'add+'

--- app/assets/javascripts/add-modal.js
+++ app/assets/javascripts/add-modal.js
@@ -1,12 +1,26 @@
 var AddModalView = {
-  show: function() {    
-    var modal = $("#add-modal");
+  getNodeId: function(app) {
+    var node = $("#add-modal");
+    if (app == "scrap") {
+      node = $("#scrap-trip-modal");
+    } else if (app == "upload") {
+      node = $("#upload-trip-modal");
+    } else if (app == "bundle") {
+      node = $("#bundle-trip-modal");
+    }
+    return node
+  },
+  show: function(app) {        
+    var modal = this.getNodeId(app)
     modal.css("display", "block");
     modal.css("-webkit-transform", "none");
   },
-  close: function() {
-    var modal = $("#add-modal");
+  close: function(app, child) {
+    var modal = this.getNodeId(app)
     modal.css("display", "none");
-    modal.css("-webkit-transform", "translateZ(0)");
+    modal.css("-webkit-transform", "translateZ(0)");    
+    if (child != null) {
+      this.show(child)
+    } 
   }
 }
